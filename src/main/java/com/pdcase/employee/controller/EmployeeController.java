c45433e1796c25cf5fcbe2d02d47c62a94a665d8
juanpereira2 <juanpereira74@hotmail.com>
2021-03-05 08:49:58 -0800

    ajuste em rotas get e post

--- src/main/java/com/pdcase/employee/controller/EmployeeController.java
+++ src/main/java/com/pdcase/employee/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,18 +31,18 @@ public class EmployeeController {
 	}
 
 	// end::get-aggregate-root[]
-	@PostMapping("/employees")
+	@PostMapping("/employee")
 	Employee newEmployee(@RequestBody Employee newEmployee) {
 		return repository.save(newEmployee);
 	}
 
 	@GetMapping("/employee/{id}")
-	Employee selecionarEmployee(@RequestBody Long id) {
+	Employee selecionarEmployee(@PathVariable Long id) {
 		return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 
 	}
-
-	Employee SubstituirEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+	@PutMapping("/employee/{id}")
+	 Employee SubstituirEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 		return repository.findById(id).map(employee -> {
 			employee.setName(newEmployee.getName());
 			employee.setRole(newEmployee.getRole());
@@ -52,7 +53,7 @@ public class EmployeeController {
 			return repository.save(newEmployee);
 		});
 	}
-	 @DeleteMapping("/employees/{id}")
+	 @DeleteMapping("/employee/{id}")
 	  void deleteEmployee(@PathVariable Long id) {
 	    repository.deleteById(id);
 	  }
